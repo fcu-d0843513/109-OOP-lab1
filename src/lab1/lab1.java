bac24a4d30319908ae4a60844387f41421373602
user <user@EF08>
2020-09-29 09:36:29 +0800

    this is lab1 hw

---
+++ src/lab1/lab1.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class lab1 {
+	
+	public static void main(String[] args){
+		System.out.println("I'm Tim");
+		System.out.println("My nid is D0843513");
+		System.out.println("This is lab1 HW");
+	}
+	
+}
